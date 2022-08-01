fefe10a59239111c9d09315667a6b8564df2f948
peckover <56835853+peckover@users.noreply.github.com>
2022-08-01 12:04:29 +0100

    [DLS-5550] remove unused simple-reactive-mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,8 +8,7 @@ object AppDependencies {
   val bootstrapVersion = "6.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc"             %% "simple-reactivemongo"       % "8.1.0-play-28"
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion
   )
 
   val test = Seq(
