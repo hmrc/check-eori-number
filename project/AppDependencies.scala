e89dbc809d44bcfea711edbd01cc09fef0bbbeeb
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2022-10-21 15:57:29 +0100

    DLS-6023 backend bootstrap upgrade (#16)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "6.4.0"
+  val bootstrapVersion = "7.8.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % bootstrapVersion
@@ -15,7 +15,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion                 % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.12"                 % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.0"               % "test, it",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2"               % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                 % "test, it"
   )
 }
