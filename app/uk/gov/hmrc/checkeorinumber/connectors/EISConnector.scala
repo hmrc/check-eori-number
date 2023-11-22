ff1a9df5e647f4c0a5f1bee4a4eb0934a0ab82d4
Graeme Stirling <graemejstirling@gmail.com>
2023-11-22 10:07:21 +0000

    formattting

--- app/uk/gov/hmrc/checkeorinumber/connectors/EISConnector.scala
+++ app/uk/gov/hmrc/checkeorinumber/connectors/EISConnector.scala
@@ -36,8 +36,8 @@ trait EISConnector {
 
   def checkEoriNumbers(
     checkRequest: CheckMultipleEoriNumbersRequest
-  )(
-    implicit hc: HeaderCarrier,
+  )(implicit
+    hc: HeaderCarrier,
     ec: ExecutionContext
   ): Future[List[CheckResponse]]
 
@@ -50,7 +50,7 @@ trait EISJsonConverter {
 
     override def reads(json: JsValue): JsResult[CheckResponse] = {
       val basePath = json \ "identifications"
-      val eori = (basePath \ "eori").as[String]
+      val eori     = (basePath \ "eori").as[String]
 
       JsSuccess(
         CheckResponse(
@@ -64,12 +64,12 @@ trait EISJsonConverter {
             case (Some(_), None, None) =>
               logger.warn(s"traderName found but address is empty for [$eori]")
               None
-            case (None, _ ,Some(_))=>
+            case (None, _, Some(_)) =>
               logger.warn(s"address found but traderName is empty for [$eori]")
               None
             case (Some(a), Some(b), _) =>
               Some(CompanyDetails(a, b))
-            case (Some(a),None, Some(b)) =>
+            case (Some(a), None, Some(b)) =>
               Some(CompanyDetails(a, Address(b.streetAndNumber, b.cityName, "")))
             case (None, None, None) => None
           }
@@ -82,44 +82,45 @@ trait EISJsonConverter {
 }
 
 @Singleton
-class EISConnectorImpl @Inject()(
+class EISConnectorImpl @Inject() (
   http: HttpClient,
   servicesConfig: ServicesConfig
-) extends EISConnector with EISJsonConverter {
+) extends EISConnector
+    with EISJsonConverter {
 
-   private val eisURL = s"${servicesConfig.baseUrl("eis")}"
+  private val eisURL = s"${servicesConfig.baseUrl("eis")}"
 
-   private def addHeaders(implicit hc: HeaderCarrier): HeaderCarrier = {
+  private def addHeaders(implicit hc: HeaderCarrier): HeaderCarrier = {
 
-     //HTTP-date format defined by RFC 7231 e.g. Fri, 01 Aug 2020 15:51:38 GMT+1
-     val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss O")
+    // HTTP-date format defined by RFC 7231 e.g. Fri, 01 Aug 2020 15:51:38 GMT+1
+    val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss O")
 
-     hc.withExtraHeaders(
-       "X-Forwarded-Host" -> "mdtp",
-       "X-Correlation-ID" -> UUID.randomUUID().toString,
-       HeaderNames.DATE -> ZonedDateTime.now().format(formatter),
-       HeaderNames.CONTENT_TYPE -> ContentTypes.JSON,
-       HeaderNames.ACCEPT -> ContentTypes.JSON,
-       "Environment" -> s"${servicesConfig.getConfString("eis.environment", "")}"
-     ).copy(
-       authorization = None
-     )
-   }
+    hc.withExtraHeaders(
+      "X-Forwarded-Host"       -> "mdtp",
+      "X-Correlation-ID"       -> UUID.randomUUID().toString,
+      HeaderNames.DATE         -> ZonedDateTime.now().format(formatter),
+      HeaderNames.CONTENT_TYPE -> ContentTypes.JSON,
+      HeaderNames.ACCEPT       -> ContentTypes.JSON,
+      "Environment"            -> s"${servicesConfig.getConfString("eis.environment", "")}"
+    ).copy(
+      authorization = None
+    )
+  }
 
-   def checkEoriNumbers(
-     checkRequest: CheckMultipleEoriNumbersRequest
-   )(
-     implicit hc: HeaderCarrier,
-     ec: ExecutionContext
-   ): Future[List[CheckResponse]] = {
+  def checkEoriNumbers(
+    checkRequest: CheckMultipleEoriNumbersRequest
+  )(implicit
+    hc: HeaderCarrier,
+    ec: ExecutionContext
+  ): Future[List[CheckResponse]] = {
 
-     val url = s"$eisURL/gbeorichecker/gbeorirequest/v1"
-     val json = http.POST[CheckMultipleEoriNumbersRequest, JsObject](
-       url,
-       checkRequest,
-       Seq("Authorization" -> s"Bearer ${servicesConfig.getConfString("eis.token", "")}")
-     )(implicitly, implicitly, addHeaders, ec)
+    val url = s"$eisURL/gbeorichecker/gbeorirequest/v1"
+    val json = http.POST[CheckMultipleEoriNumbersRequest, JsObject](
+      url,
+      checkRequest,
+      Seq("Authorization" -> s"Bearer ${servicesConfig.getConfString("eis.token", "")}")
+    )(implicitly, implicitly, addHeaders, ec)
 
-     json.map(x => (x \ "party").as[List[CheckResponse]])
-   }
+    json.map(x => (x \ "party").as[List[CheckResponse]])
+  }
 }
