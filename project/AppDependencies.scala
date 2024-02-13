fc6e1c863e52c037c0204c8a534c504e4a8a1437
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2024-02-13 17:08:13 +0000

    DDCYLS-4060b: Update play versions to 2.9

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ object AppDependencies {
   val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-29" % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-29" % bootstrapVersion % Test,
     "org.scalatest"          %% "scalatest"              % "3.2.15"         % Test,
     "com.typesafe.play"      %% "play-test"              % current          % Test,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % "test, it",
