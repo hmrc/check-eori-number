8ddccaae96eda38b9094b7bd8549b4b18e5b6181
liam.henderson <35490828+liamhenderson92@users.noreply.github.com>
2024-02-13 16:54:16 +0000

    WIP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,14 +6,14 @@ object AppDependencies {
   val bootstrapVersion = "8.4.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % bootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.scalatest"          %% "scalatest"              % "3.2.15"         % Test,
     "com.typesafe.play"      %% "play-test"              % current          % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % "test, it",
+    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.6"         % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "6.0.0"          % "test, it"
   )
 }
